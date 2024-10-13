0a27dd6b229ede149b4621e7eb1c74bd7b8790ef
Usama Ansari <usmslm102@gmail.com>
2024-10-14 02:04:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WhatSend/',
+  base: '/',
   plugins: [react(),
     VitePWA({
       registerType: 'autoUpdate',
